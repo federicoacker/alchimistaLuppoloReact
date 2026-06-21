cc89ec77e54fa6ba0148259986f7773fc3160ec3
giulia-buonasperanza <giulia.9402@gmail.com>
2026-06-21 23:56:59 +0200

    Aggiunta header.module e logo senza scritte, modifiche navbar. Piccolo fix nella HeroSection

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,29 +2,44 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import styles from './Header.module.css';
+import { useState } from 'react';
 
 function Header() {
+    const [active, setActive] = useState("home");
 
     return (
         <>
-            <h1>Headerrssss</h1>
-            <div className="div-navbar">
-                <Navbar expand="lg" className="bg-body-tertiary">
-                    <Container>
-                        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="me-auto">
-                                <Nav.Link href="#home">Home</Nav.Link>
-                                <Nav.Link href="#birre">Birre</Nav.Link>
-                                <Nav.Link href="#novità">Novità</Nav.Link>
-                                <Nav.Link href='#contatti'>Contatti</Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-            </div>
+
+            <Navbar expand="lg" className={styles.customNavbar} variant="dark">
+                <Container>
+                    <Navbar.Brand href="/" className={styles.logoContainer}>
+                        <div className={styles.logoGlow}></div>
+                        <img src="/imgs/logo-semplice.png" alt="L'Alchimista del Luppolo" className={styles.navbarLogo} />
+                    </Navbar.Brand>
+                    <a href="/carrello" className={styles.cartIconMobile}><i className="bi bi-cart3"></i> </a>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home"
+                                active={active === "home"}
+                                className={active === "home" ? styles.activeLink : ''} onClick={() => setActive("home")}>
+                                Home</Nav.Link>
+                            <Nav.Link href="#birre" active={active === "birre"}
+                                className={active === "birre" ? styles.activeLink : ''} onClick={() => setActive("birre")}>
+                                Birre</Nav.Link>
+                            <Nav.Link href="#novità" active={active === "novità"}
+                                className={active === "novità" ? styles.activeLink : ''} onClick={() => setActive("novità")}>
+                                Novità</Nav.Link>
+                            <Nav.Link href='#contatti' active={active === "contatti"}
+                                className={active === "contatti" ? styles.activeLink : ''} onClick={() => setActive("contatti")}>
+                                Contatti</Nav.Link>
+                        </Nav>
+                        <a href="/carrello" className={styles.cartIconDesktop}><i className="bi bi-cart3"></i></a>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
         </>
     )
 }
