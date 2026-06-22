7ff15c099d1be198ec630cd465821d0aad2bb0d6
federicoacker <acker.federico@gmail.com>
2026-06-22 13:42:40 +0200

    Aggiunto useProduct per fare il fetch dei prodotti singoli

--- src/components/NewBeers.jsx
+++ src/components/NewBeers.jsx
@@ -8,13 +8,11 @@ import styles from "./NewBeers.module.css";
 
 function NewBeers() {
     const { products, loading, error } = useProducts("?limit=6&orderBy=created_at&excluded-brewery=L'Alchimista%20Del%20Luppolo")
-    console.log(products);
-    console.log(error);
     return (
         <>
             <h2 className={styles["section-title"]}>Le nuove arrivate</h2>
             <Row className="row-gap-4">
-                {products.map(product => <BeerCard key={product.slug} product={product} />)}
+                {!loading && !error && products.map(product => <BeerCard key={product.slug} product={product} />)}
             </Row>
         </>
     )
