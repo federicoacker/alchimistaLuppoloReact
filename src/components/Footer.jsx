6e429b8746db26e9820e9a81745f7e88c858d7bf
giulia-buonasperanza <giulia.9402@gmail.com>
2026-06-22 13:00:17 +0200

    aggiunto footer.module e impostato la base del footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,21 @@
+import styles from "./Footer.module.css";
+
 function Footer() {
 
     return (
-        <>
-        <h1>FOOTER</h1>
-        </>
-    )
+        <footer className={styles.footer}>
+            <div className={styles.footerDivider}>
+                <span>🜂</span>
+                <span>🜄</span>
+                <span>🜁</span>
+                <span>🜃</span>
+            </div>
+
+            <div className={styles.footerContent}>
+                <h5>L'Alchimista del Luppolo</h5>
+                <p>Malto + Luppolo + Lievito + Tempo = Magia</p>
+            </div>
+        </footer>
+    );
 }
 export default Footer;
