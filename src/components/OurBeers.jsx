65e156c5ec1e3af5b0bf45ebaf5aa313d0a903d4
federicoacker <acker.federico@gmail.com>
2026-06-22 13:42:54 +0200

    fix di un piccolo typo

--- src/components/OurBeers.jsx
+++ src/components/OurBeers.jsx
@@ -7,13 +7,12 @@ import { Row } from 'react-bootstrap';
 
 function OurBeers() {
   const { products, loading, error } = useProducts("?limit=6&brewery=L'Alchimista del Luppolo")
-  console.log(products);
-  console.log(error);
   return (
     <>
       <h2 className={styles["section-title"]}>La nostra produzione</h2>
       <Row className="row-gap-4">
-        {products.map(product => <BeerCard key={product.slug} product={product} />)}
+
+        {!loading && !error && products.map(product => <BeerCard key={product.slug} product={product} />)}
       </Row>
     </>
   )
