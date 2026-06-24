eea83c998d4351ffa32c4a812747f9fbca735039
federicoacker <acker.federico@gmail.com>
2026-06-24 12:11:33 +0200

    Aggiustate le card

--- src/components/BeerCardHorizontal.jsx
+++ src/components/BeerCardHorizontal.jsx
@@ -1,8 +1,17 @@
 import { Button } from "react-bootstrap";
 import { Link } from "react-router";
 import styles from "./BeerCardHorizontal.module.css";
+import { useWishlist } from "../contexts/WishlistContext";
+import useCart from "../hooks/useCart";
 
 function BeerCardHorizontal({ product }) {
+    const {
+        toggleWishlist,
+        isInWishlist
+    } = useWishlist();
+    const favorite = isInWishlist(product.slug);
+    const { cartItems, addToCart } = useCart();
+    console.log(cartItems);
     return (
         <div className={styles["beer-card"]}>
             <div className="d-flex w-100 flex-wrap">
@@ -15,8 +24,13 @@ function BeerCardHorizontal({ product }) {
                     <p className={styles["beer-card-text"]}>IBU: {product.ibu}</p>
                 </div>
                 <div className={`d-flex gap-4 justify-content-center ${styles["beer-card-buttons"]}`}>
-                    <Button className={styles["beer-button"]}>Ordina</Button>
-                    <Link to={`/products/${product.slug}`} className={`btn ${styles["beer-button"]} d-flex flex-column justify-content-center`}>Dettagli</Link>
+                    <Button className={`${styles["icon-button"]} ${styles["beer-button"]} `} onClick={() => { addToCart(product) }}><i className="bi bi-cart-fill"></i></Button>
+                    <Button className={`${styles["icon-button"]} ${styles["beer-button"]} `}
+                        onClick={() => toggleWishlist(product.slug)}>
+                        {favorite ? (<i className="bi bi-heart-fill"></i>)
+                            : (<i className="bi bi-heart"></i>)}
+                    </Button>
+                    <Link to={`/products/${product.slug}`} className={`btn ${styles["beer-button"]} ${styles["text-button"]}`}>Dettagli</Link>
                 </div>
 
             </div>
