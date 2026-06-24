ca828e739bde0f3367900e288745c5e60eacc360
federicoacker <acker.federico@gmail.com>
2026-06-24 13:07:56 +0200

    Finito l'offcanvas

--- src/components/CartOffCanvas.jsx
+++ src/components/CartOffCanvas.jsx
@@ -1,27 +1,60 @@
 
 import useCart from "../hooks/useCart"
 import { Offcanvas } from "react-bootstrap";
-import Section from "./Section";
+import { Link } from "react-router";
 import BeerCardHorizontal from "./BeerCardHorizontal";
 import styles from "./CartOffCanvas.module.css";
 
 
 function CartOffCanvas() {
-    const { cartItems, show, handleClose } = useCart();
+    const { cartItems, show, handleClose, productsPrice, shippingPrice, totalPrice } = useCart();
     console.log(cartItems);
     return (
-        <Offcanvas show={show} onHide={handleClose} placement="end" className={styles["cart-offcanvas"]}>
+        <Offcanvas show={show} onHide={handleClose} placement="end" className={`${styles["cart-offcanvas"]} ${styles["section-frame"]}`}>
             <Offcanvas.Header closeButton closeVariant="white">
                 <Offcanvas.Title className={styles["cart-title"]}>Carrello</Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
-                <Section className={styles["cart-section"]}>
+                <section className={styles["cart-section"]}>
                     <div className="d-flex flex-column">
                         {cartItems.map((cartItem) => {
                             return <BeerCardHorizontal key={cartItem.cartProduct.slug} product={cartItem.cartProduct} quantity={cartItem.quantity} />
                         })}
                     </div>
-                </Section>
+                    {cartItems.length > 0 &&
+                        <div>
+                            <table className={styles["cart-total"]}>
+                                <thead>
+                                    <tr>
+                                        <th className={styles["cart-tag"]}>
+                                            Prezzo Prodotti
+                                        </th>
+                                        <th className={styles["cart-tag"]}>
+                                            Prezzo di Shipping
+                                        </th>
+                                        <th colspan={2} className={styles["cart-tag"]}>
+                                            Totale
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td className={styles["cart-tag"]}>
+                                            &euro; {productsPrice.toFixed(2)}
+                                        </td>
+                                        <td className={styles["cart-tag"]}>
+                                            &euro; {shippingPrice.toFixed(2)}
+                                        </td>
+                                        <td colspan={2} className={styles["cart-tag"]}>
+                                            &euro; {totalPrice.toFixed(2)}
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <Link to="/checkout" className={`my-2 d-block mx-auto btn ${styles["beer-button"]} ${styles["text-button"]}`}>Vai al checkout</Link>
+                        </div>
+                    }
+                </section>
             </Offcanvas.Body>
         </Offcanvas>
     )
