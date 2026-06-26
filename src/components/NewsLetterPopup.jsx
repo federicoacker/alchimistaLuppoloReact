6683bc1cca9bbe9c0790a5e41d78c949bea8f99e
federicoacker <acker.federico@gmail.com>
2026-06-26 14:00:53 +0200

    hotfix rimosso import superfluo

--- src/components/NewsLetterPopup.jsx
+++ src/components/NewsLetterPopup.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Modal, Button } from "react-bootstrap";
 import Newsletter from "./Newsletter"
 import styles from "./NewsLetterPopup.module.css";
