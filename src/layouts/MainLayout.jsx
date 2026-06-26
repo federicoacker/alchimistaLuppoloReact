83aaf7369f29cece3bb260c55a321a63ad35cb7b
federicoacker <acker.federico@gmail.com>
2026-06-26 13:59:20 +0200

    Fatto il popup per la newsletter

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,6 +3,7 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 import styles from './MainLayout.module.css'
 import CartOffCanvas from '../components/CartOffCanvas'
+import NewsLetterPopup from '../components/NewsLetterPopup'
 
 
 function MainLayout() {
@@ -11,6 +12,7 @@ function MainLayout() {
         <>
             <Header />
             <main className={styles.mainLayout}>
+                <NewsLetterPopup/>
                 <CartOffCanvas/>
                 <Outlet />
             </main>
