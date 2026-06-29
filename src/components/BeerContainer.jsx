639cf75de4af3ca52fd9c5da61c7c84d5dd1c1a7
federicoacker <acker.federico@gmail.com>
2026-06-29 10:43:03 +0200

    Fixato piccolo bug

--- src/components/BeerContainer.jsx
+++ src/components/BeerContainer.jsx
@@ -10,29 +10,25 @@ import PageNavigator from "./PageNavigator.jsx";
 import useUrlQuery from "../hooks/useUrlQuery.js";
 
 import FiltersSection from "./FiltersSection.jsx";
-import { useNavigate } from "react-router";
+import { Navigate } from "react-router";
 
 const MAX_ITEMS_PER_PAGE = 9;
 function BeerContainer() {
     const urlQueryObject = useUrlQuery(MAX_ITEMS_PER_PAGE)
-    const {debouncedQuery, selectedCategoryArray, orderBy, order, offset, setOffset} = urlQueryObject;
+    const { debouncedQuery, selectedCategoryArray, orderBy, order, offset, setOffset } = urlQueryObject;
     const { products, loading, productCount } = useProducts(`?search=${debouncedQuery}&category=${selectedCategoryArray.join(",")}&orderBy=${orderBy}&order=${order}&limit=${MAX_ITEMS_PER_PAGE}&offset=${offset}`, true);
     const [isGrid, setIsGrid] = useState(true);
-    const navigate = useNavigate();
 
-    if(!loading && products.length === 0){
-        navigate("/404");
-    }
     return (
         <Section className="gap-4">
             <Section className={`d-flex ${styles["search-section"]}`}>
-                <FiltersSection urlQueryObject={urlQueryObject} setIsGrid={setIsGrid} isGrid={isGrid}/>
+                <FiltersSection urlQueryObject={urlQueryObject} setIsGrid={setIsGrid} isGrid={isGrid} />
             </Section>
             <p>Sono stati trovati {productCount} risultati</p>
             <PageNavigator currentOffset={offset} MAX_ITEMS_PER_PAGE={MAX_ITEMS_PER_PAGE} setOffset={setOffset} productCount={productCount} />
             <Row className={`row-gap-4 ${styles["product-section"]}`}>
-                {
-                    !loading && products.map(product => { return isGrid ? <BeerCardVertical key={product?.slug} product={product} /> : <BeerCardHorizontal key={product?.slug} product={product} /> })
+                {(!loading && products.length === 0) ? <Navigate to="/404" /> :
+                    products.map(product => { return isGrid ? <BeerCardVertical key={product?.slug} product={product} /> : <BeerCardHorizontal key={product?.slug} product={product} /> })
                 }
             </Row>
         </Section>
